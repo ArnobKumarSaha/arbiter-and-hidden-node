7581d4ef3bb2b7bd2e661d9a4b63f5b76de940a0
Arnob kumar saha <arnob@appscode.com>
2022-08-10 19:30:40 +0600

    careful to ping the members

--- hidden/run.sh
+++ hidden/run.sh
@@ -1,6 +1,15 @@
 
 #!/bin/bash
 
+HOSTNAME=sh-shard0-hidden-2
+HIDDEN_COUNT=4
+for (( c=1; c<=$HIDDEN_COUNT; c++ )); do
+    cur_host=$(echo $HOSTNAME | sed -e "s/[-][0-9]*$/-$c/")  
+    # cur_host=${HOSTNAME//[-][0-9]*/}
+    echo $c $cur_host
+done
+
+exit 0
 
 conf=$(cat actual.conf)
 #conf=$(cat modified.conf)
@@ -10,7 +19,7 @@ echo "$conf" > conf.json
 
 for var in "NumberLong" "ObjectId";do 
     echo $var 
-    sed -i  -e "/$var/s/[)]//" -e "/$var/s/$var[(]//" conf.json
+    sed -i  -e "/$var/s/[)]//" -e "/$var/s/${var}[(]//" conf.json
     # sed -i  -e '/NumberLong/s/NumberLong[(]//' conf.json
 done
 
@@ -20,7 +29,7 @@ x=$(cat conf.json | jq '.members[]')
 # x=$(echo "$conf" | jq -r '.settings')
 # echo "$x"
 
-service_name=mg-rs-hidden-0.mg-rs-pods.demo.svc.cluster.local:27017
+service_name=mg-rs-hidden-0.mg-rs-pods.demo.svc.cluster.local
 
 readarray -t my_array < <(jq -c '.members[]' conf.json)
 for item in "${my_array[@]}"; do
@@ -30,12 +39,15 @@ for item in "${my_array[@]}"; do
   # remove the quotation marks from the start & end
   host=$(echo $host | sed -e 's/^"//' -e 's/"$//')
   echo "$host" "$hidden"
+  # host=$(echo $host | sed -e 's/[:][0-9]*//')
+  host=${host//[:][0-9]*/}
+  host=${host//[:][0-9]*/}
+  echo "$host"
 
   if [[ "$host" == "$service_name" && "$hidden" == true ]]; then
     echo "yaaa"
   fi 
 done
-
 
 
 rm conf.json
